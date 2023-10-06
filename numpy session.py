8a92d9b89f0daf0a117b3b0aa76d8bd723277cad
Ajo J Thomas <146969160+ajojosephthomas@users.noreply.github.com>
2023-10-06 14:21:00 +0100

    tutorial wk1

--- numpy session.py
+++ numpy session.py
@@ -1,1 +1,18 @@
-#testing 
+import numpy as np
+
+new_matrix = np.array([[1,2,3], [4,5,6],[7,8,9]])
+print(new_matrix)
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr[0])
+print(arr[1])
+print(arr[2])
+print(arr[3])
+print(arr[4])
+print(arr.dtype)
+
+arr = np.array([10, 20, 30, 40, 50])
+print(arr[0:4])
+
+arr = np.array([1, 2, 3, 4], ndmin=3) #numpy array with 2dimensions
