c8c2bbc09ff69186728fa2f6d4aa68f03aaa2fd1
Ajo J Thomas <146969160+ajojosephthomas@users.noreply.github.com>
2023-10-06 15:28:29 +0100

    matplotlib

---
+++ matplotlib session.py
@@ -0,0 +1,16 @@
+
+import matplotlib.pyplot as plt
+
+plt.plot ([10, 20, 30, 40])
+plt.ylabel('age')
+plt.show()
+
+#plot 2
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'go')
+plt.axis((0, 6, 0, 20))
+plt.show()
+
+plt.plot([1, 2, 3, 4])
+plt.axis([1, 2, 3])
+plt.show()
+
